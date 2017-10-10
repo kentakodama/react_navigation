e2acbfc61ec948a873d040070a0af2fdbfbef1bb
Logan Cooper <ldtcoop@gmail.com>
2017-10-10 13:09:27 -0700

    Remove doubled up configs

--- android/app/src/main/java/com/reactnavigation2/MainApplication.java
+++ android/app/src/main/java/com/reactnavigation2/MainApplication.java
@@ -8,7 +8,6 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.gettipsi.stripe.StripeReactPackage;
 
 import java.util.Arrays;
 import java.util.List;
